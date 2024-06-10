e719d2a20556825e91423db57fd0453b7a40a9a8
JasserBA <benabdallahjasserr@gmail.com>
2024-06-10 12:56:52 +0100

    add sorting-tasks by ('input','title','done')

--- Section-6/task-list/src/components/Footer.js
+++ Section-6/task-list/src/components/Footer.js
@@ -1,10 +1,18 @@
 import React from "react";
 
 const Footer = ({ nbTasks, nbDoneTasks, percentage }) => {
+  if (nbTasks===0){
+    return (
+      <footer className="stats">
+        <em>Start adding some tasks to your task list 📋.</em>
+      </footer>
+    );
+
+  }
   return (
     <footer className="stats">
       <em>
-        {nbTasks === 0
+        {percentage === 100
           ? "You have completed all tasks, go chill out!"
           : `You have ${nbTasks} tasks on your list, and you have completed ${nbDoneTasks} (${percentage}%)`}
       </em>
--- Section-6/task-list/src/components/TaskLists.js
+++ Section-6/task-list/src/components/TaskLists.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Task from "./Task";
 
 const TaskLists = ({
@@ -7,23 +7,40 @@ const TaskLists = ({
   onClearAllTasks,
   onToggleItem,
 }) => {
+
+  const [sortBy,setSortBy] = useState("input");
+
+  let sortedTasks;
+
+  if (sortBy==="input") sortedTasks = taskData;
+
+  if (sortBy === "title")
+    // The localeCompare() method compares two strings in the current locale.
+    // returns sort order -1, 1, or 0 (for before, after, or equal).
+    sortedTasks = taskData
+      .slice()
+      .sort((a, b) => a.title.localeCompare(b.title));
+
+  if(sortBy==="done")
+    sortedTasks=taskData.slice().sort((a,b)=>Number(a.completed)-Number(b.compleeted));
+
   return (
     <div className="list">
       <ul>
-        {taskData.map((t) => (
-            <Task
-              task={t}
-              key={t.id}
-              onDeleteTask={onDeleteTask}
-              onToggleItem={onToggleItem}
-            />
+        {sortedTasks.map((t) => (
+          <Task
+            task={t}
+            key={t.id}
+            onDeleteTask={onDeleteTask}
+            onToggleItem={onToggleItem}
+          />
         ))}
       </ul>
       <div className="actions">
-        <select>
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
           <option value="input">Sort by input order</option>
-          <option value="description">Sort by description</option>
-          <option value="packed">Sort by packed status</option>
+          <option value="title">Sort by title</option>
+          <option value="done">Sort by listed status</option>
         </select>
         <button onClick={onClearAllTasks}>Clear list 🗑️</button>
       </div>
