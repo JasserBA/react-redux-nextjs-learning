e657f165937c967028f8b61046a0e9fb5a5081ad
Jasser BenAbdallah <Benabdallahjasserr@gmail.com>
2024-12-22 15:06:00 +0100

    Fix: Replace Movies LIst using component into component composition

--- section-10/useforja/src/App.js
+++ section-10/useforja/src/App.js
@@ -58,7 +58,12 @@ export default function App() {
         <Search />
         <NumResults movies={movies} />
       </Navbar>
-      <Main movies={movies} />
+      <Main>
+        <ListBox>
+          <MoviesList movies={movies} />
+        </ListBox>
+        <WatchedBox />
+      </Main>
     </>
   );
 }
@@ -103,13 +108,8 @@ function NumResults({ movies }) {
     </p>
   );
 }
-function Main({ movies }) {
-  return (
-    <main className="main">
-      <ListBox movies={movies} />
-      <WatchedBox />
-    </main>
-  );
+function Main({ children }) {
+  return <main className="main">{children}</main>;
 }
 function MoviesList({ movies }) {
   return (
@@ -136,7 +136,7 @@ function Movie({ movie }) {
   );
 }
 
-function ListBox({ movies }) {
+function ListBox({ children }) {
   const [isOpen1, setIsOpen1] = useState(true);
 
   return (
@@ -147,7 +147,7 @@ function ListBox({ movies }) {
       >
         {isOpen1 ? "–" : "+"}
       </button>
-      {isOpen1 && <MoviesList movies={movies} />}
+      {isOpen1 && children}
     </div>
   );
 }
