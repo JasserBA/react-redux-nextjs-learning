ff675e7999dcf1fa89cd8fe65a571e74bbb4e3da
Jasser BenAbdallah <Benabdallahjasserr@gmail.com>
2024-12-28 20:41:44 +0100

    Update: Improving Reusability with Props

--- section-10/useforja/src/index.js
+++ section-10/useforja/src/index.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import StarRating from "./StarRating";
 // import "./index.css";
 // import App from "./App";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating
+        color="pink"
+        maxRating={10}
+        onSetRating={setMovieRating}
+        defaultRating={5}
+      />
+      <p>This Movie was rated {movieRating} stars.</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
     <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
+    <StarRating maxRating={14} color="red" />
+    <StarRating maxRating={10} color="#fffa86" className="test" />
+    <StarRating messages={["Terrible", "Bad", "Okay", "Amazing"]} />
+    <StarRating defaultRating={3} />
+    <Test />
   </React.StrictMode>
 );
