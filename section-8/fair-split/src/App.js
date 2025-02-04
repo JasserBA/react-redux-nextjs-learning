345891a728478ce80543f41ad9c31e9156967c2c
JasserBA <jasserbenabdallah6@gmail.com>
2025-02-04 22:29:38 +0100

    fix

--- section-8/fair-split/src/App.js
+++ section-8/fair-split/src/App.js
@@ -52,6 +52,7 @@ function App() {
         <FormSplit
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
+          key={selectedFriend.id} //
         />
       )}
     </div>
