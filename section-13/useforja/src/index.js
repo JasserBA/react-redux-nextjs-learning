09fbeffb7b96e84779c19efb0f4b25123177ffbc
JasserBA <jasserbenabdallah6@gmail.com>
2025-07-12 14:11:32 +0100

    added Rules of hooks / More details of useState like Avgerage Rating

---
+++ section-13/useforja/src/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AppV2 from "./App-v2";
+import "./index.css";
+// import App from "./App";
+/*
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating
+        color="pink"
+        maxRating={10}
+        onSetRating={setMovieRating}
+        defaultRating={5}
+      />
+      <p>This Movie was rated {movieRating} stars.</p>
+    </div>
+  );
+}
+*/
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <>
+    {/* removed StrictMode to avoid double useEffect */}
+    <AppV2 />
+    {/* <StarRating maxRating={"5"} />
+    <StarRating maxRating={14} color="red" />
+    <StarRating maxRating={10} color="#fffa86" className="test" />
+    <StarRating messages={["Terrible", "Bad", "Okay", "Amazing"]} />
+    <StarRating defaultRating={3} />
+    <Test /> */}
+  </>
+);
