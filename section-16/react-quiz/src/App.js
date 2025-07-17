c59eb13cd1e3bc25389d829131ad725982110a1b
JasserBA <jasserbenabdallah6@gmail.com>
2025-07-17 17:09:19 +0100

    Added json-server for API data and updated App.js to fetch data from json-server (with useReduce)

--- section-16/react-quiz/src/App.js
+++ section-16/react-quiz/src/App.js
@@ -1,8 +1,43 @@
+import { useEffect, useReducer } from "react";
 import "./App.css";
 import Header from "./Header";
 import { Main } from "./Main";
+const initialState = {
+  questions: [],
 
+  // "loading", "error", "ready", "active", "finished"
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Action unknown");
+  }
+}
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        dispatch({ type: "dataReceived", payload: data });
+      })
+      .catch((error) => {
+        console.error("Error fetching questions:", error);
+        dispatch({ type: "dataFailed" });
+      });
+  }, []);
+
   return (
     <div className="app">
       <Header />
@@ -10,7 +45,7 @@ function App() {
         <p>1/15</p>
         <p>Question?</p>
       </Main>
-      {/* <DateCounter /> */}
+      {/* <DateCounte r /> */}
     </div>
   );
 }
