f693a8ab86d722cad5b713fc47e1cefa6bfbe568
JasserBA <jasserbenabdallah6@gmail.com>
2025-07-30 20:59:41 +0100

    Including a Map With the Leaflet Library

--- section-17/worldwise/src/components/Map.jsx
+++ section-17/worldwise/src/components/Map.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Map.module.css";
 import { useNavigate, useSearchParams } from "react-router-dom";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 export const Map = () => {
   const navigate = useNavigate();
+  const [mapPosition, _setMapPosition] = useState([19, 19]);
   const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  const _lat = searchParams.get("lat");
+  const _lng = searchParams.get("lng");
   setSearchParams;
   return (
     <div
@@ -15,17 +17,22 @@ export const Map = () => {
         navigate("form");
       }}
     >
-      <h1>Map</h1>
-      <h2>
-        Position : {lat}, {lng}
-      </h2>
-      <button
-        onClick={() => {
-          setSearchParams({ lat: 23, lng: 50 });
-        }}
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={false}
+        className={styles.map}
       >
-        Change
-      </button>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 };
