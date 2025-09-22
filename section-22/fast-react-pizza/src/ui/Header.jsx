debf6b4e7b36302fb2b29e968200bf491d3416c3
jasserba <benabdallahjasserr@gmail.com>
2025-09-22 20:42:18 +0100

    feat: enhance UI components with Tailwind CSS styles and add Username component

--- section-22/fast-react-pizza/src/ui/Header.jsx
+++ section-22/fast-react-pizza/src/ui/Header.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { SearchOrder } from "../features/order/SearchOrder";
+import { Username } from "../features/user/Username";
 
 export const Header = () => {
   return (
-    <header>
-      <Link to="/">Fast React Pizza Co.</Link>
+    <header className="bg-red-500 p-4 flex justify-between items-center ">
+      <Link to="/" className="uppercase tracking-[0.3em] text-white font-bold">
+        Fast React Pizza Co.
+      </Link>
       <SearchOrder />
+      <Username />
     </header>
   );
 };
