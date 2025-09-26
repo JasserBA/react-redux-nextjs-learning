8f52aeea50b58683b7bf182aa1d730c62189dbe4
jasserba <benabdallahjasserr@gmail.com>
2025-09-26 21:36:51 +0100

    Update index.html, Header.jsx, Loader.jsx and tailwind.config.js with minor changes

--- section-22/fast-react-pizza/src/ui/Loader.jsx
+++ section-22/fast-react-pizza/src/ui/Loader.jsx
@@ -1,6 +1,6 @@
 export const Loader = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-stone-100">
+    <div className="absolute inset-0 flex items-center justify-center bg-stone-100/80 z-50">
       <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
     </div>
   );
