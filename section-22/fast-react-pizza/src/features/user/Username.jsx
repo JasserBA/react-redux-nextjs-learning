2bae6b0482662c932b5712cecfd321a4250bfc80
jasserba <benabdallahjasserr@gmail.com>
2025-09-27 20:48:07 +0100

    `feat: added react-redux and updated package-lock.json and package.json to reflect changes`

--- section-22/fast-react-pizza/src/features/user/Username.jsx
+++ section-22/fast-react-pizza/src/features/user/Username.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 export const Username = () => {
-  return <div>JASSER</div>;
+  const username = useSelector((state) => state.user.username);
+
+  if (!username) return null;
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
 };
---
+++ section-22/fast-react-pizza/src/features/user/userSlice.js
@@ -0,0 +1,50 @@
+// import { getAddress } from "../../services/apiGeocoding";
+
+// function getPosition() {
+//   return new Promise(function (resolve, reject) {
+//     navigator.geolocation.getCurrentPosition(resolve, reject);
+//   });
+// }
+
+// async function fetchAddress() {
+//   // 1) We get the user's geolocation position
+//   const positionObj = await getPosition();
+//   const position = {
+//     latitude: positionObj.coords.latitude,
+//     longitude: positionObj.coords.longitude,
+//   };
+
+//   // 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
+//   const addressObj = await getAddress(position);
+//   const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
+
+//   // 3) Then we return an object with the data that we are interested in
+//   return { position, address };
+// }
+
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  username: "dsfsdf",
+  phone: "",
+  address: "",
+  position: null,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      state.username = action.payload.username;
+      state.phone = action.payload.phone;
+      state.address = action.payload.address;
+      state.position = action.payload.position;
+    },
+  },
+});
+
+export const { updateName, updatePhone, updateAddress, setUser } =
+  userSlice.actions;
+
+export default userSlice.reducer;
